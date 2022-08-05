9e0cc69eb90b6021a211f45fc81a62031dd73b41
Kilar <kpafifov@bk.ru>
2022-08-05 13:37:10 +0300

    Alredy done

---
+++ UserApp/AuthWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UserApp
+{
+    /// <summary>
+    /// Interaction logic for AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Auth_Click(object sender, RoutedEventArgs e)
+        {
+            string login = TextBoxLogin.Text.Trim();
+            string pass = PassBox.Password.Trim();
+
+            if (login.Length < 5)
+            {
+                TextBoxLogin.ToolTip = "Данные введены не корректно!";
+                TextBoxLogin.Background = Brushes.DarkRed;
+            }
+            else if (pass.Length < 5)
+            {
+                PassBox.ToolTip = "Данные введены не корректно!";
+                PassBox.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                TextBoxLogin.ToolTip = "";
+                TextBoxLogin.Background = Brushes.Transparent;
+                PassBox.ToolTip = "";
+                PassBox.Background = Brushes.Transparent;
+
+                User authUser = null;
+                using(AppContext db = new AppContext())
+                {
+                    authUser = db.Users.Where(b => b.Login == login && b.Pass == pass).FirstOrDefault();   
+                }
+                if(authUser == null)
+                {
+                    MessageBox.Show("Отлично!");
+                    UserPageWindow userPageWindow = new UserPageWindow();
+                    userPageWindow.Show();
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Не верные данные!");
+                }
+                
+            }
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+    }
+}
